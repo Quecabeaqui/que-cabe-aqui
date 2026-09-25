479745286b407b5037625bb18ab46eb8824e0c76
Quecabeaqui <irenelemars@gmail.com>
2026-09-25 11:15:54 +0200

    feat: add verified 45cm dishwasher from Awin feed

---
+++ src/priorityCatalog.ts
@@ -0,0 +1,12 @@
+export const priorityProducts = [
+  {
+    id: 'awin-37701111449',
+    title: 'Mini lavavajillas 4,5L 45cm app 6 programas',
+    url: 'https://www.awin1.com/pclick.php?p=37701111449&a=3098668&m=24018',
+    image: 'https://www.deubaxxl.de/media/66/45/f8/01_109746_on_fs_b2b_low.jpg?ts=1775716761',
+    category: 'Electrodomésticos',
+    width: 45,
+    depth: 44,
+    height: 46,
+  },
+];
